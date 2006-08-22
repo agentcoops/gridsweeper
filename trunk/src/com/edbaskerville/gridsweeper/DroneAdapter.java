1cbd5e6d679b5a42c1d302ff79df68a1358075e1
ebaskerv <ebaskerv@e466becf-6918-0410-8e4b-ca7f87a03805>
2006-08-22 23:18:52 +0000

    Drone adapter working.
    git-svn-id: svn+ssh://battistini.cscs.lsa.umich.edu/users/gsweep/svn/gridsweeper@16 e466becf-6918-0410-8e4b-ca7f87a03805

--- trunk/src/com/edbaskerville/gridsweeper/DroneAdapter.java
+++ trunk/src/com/edbaskerville/gridsweeper/DroneAdapter.java
@@ -1,5 +1,7 @@
 package com.edbaskerville.gridsweeper;
 
+import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 import com.edbaskerville.gridsweeper.parameters.ParameterMap;
@@ -52,15 +54,15 @@ public class DroneAdapter implements Adapter
 		List<String> arguments = new ArrayList<String>();
 		
 		// First, add non-parameter options
+		if(miscOptions != null)
+		{
+			arguments.addAll(StringUtils.tokenize(miscOptions));
+		}
 		arguments.add(runNumOption + runNumPrefix + runNumber);
 		arguments.add(rngSeedOption + rngSeed);
 		if(useInputFile && inputFilePath != null)
 		{
 			arguments.add(inputFileOption + inputFilePath);
-		}
-		if(miscOptions != null)
-		{
-			arguments.addAll(StringUtils.tokenize(miscOptions));
 		}
 		
 		// Now add all parameter settings
@@ -77,8 +79,52 @@ public class DroneAdapter implements Adapter
 		
 		int status = 0;
 		String message = messageBuffer.toString();
+		
 		byte[] stdoutData = null;
 		byte[] stderrData = null;
+		
+		if(!dryRun)
+		{
+			// Create command array
+			String[] cmdArray = new String[arguments.size() + 1];
+			cmdArray[0] = command;
+			for(int i = 0; i < arguments.size(); i++)
+			{
+				cmdArray[i+1] = arguments.get(i);
+			}
+			
+			try
+			{
+				Process process = Runtime.getRuntime().exec(cmdArray);
+				
+				// Write to stdin stream
+				OutputStream stdinStream = process.getOutputStream();
+				if(stdinData != null)
+				{
+					stdinStream.write(stdinData);
+				}
+				stdinStream.close();
+				
+				// Read to end of stdout and stderr streams
+				int b;
+				
+				InputStream stdoutStream = process.getInputStream();
+				ByteArrayOutputStream stdoutByteStream = new ByteArrayOutputStream();
+				while((b = stdoutStream.read()) != -1)
+					stdoutByteStream.write(b);
+				stdoutData = stdoutByteStream.toByteArray();
+				
+				InputStream stderrStream = process.getErrorStream();
+				ByteArrayOutputStream stderrByteStream = new ByteArrayOutputStream();
+				while((b = stderrStream.read()) != -1)
+					stderrByteStream.write(b);
+				stderrData = stderrByteStream.toByteArray();
+			}
+			catch (IOException e)
+			{
+				throw new AdapterException("Received IOException running process", e);
+			}
+		}
 		
 		return new RunResults(status, message, stdoutData, stderrData);
 	}
--- trunk/src/com/edbaskerville/gridsweeper/DroneAdapterTest.java
+++ trunk/src/com/edbaskerville/gridsweeper/DroneAdapterTest.java
@@ -4,6 +4,8 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 import com.edbaskerville.gridsweeper.parameters.*;
+
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 public class DroneAdapterTest
@@ -23,5 +25,23 @@ public class DroneAdapterTest
 		
 		String message = results.getMessage();
 		assertEquals("mymodel -N5 -S100 -Dbeta=0.1", message);
+	}
+	
+	@Test
+	public void testRealRunWithDefaults() throws AdapterException, UnsupportedEncodingException
+	{
+		Properties properties = new Properties();
+		properties.setProperty("command", "/bin/echo");
+		properties.setProperty("miscOptions", "-n");
+		DroneAdapter adapter = new DroneAdapter(properties, null);
+		
+		ParameterMap parameters = new ParameterMap();
+		parameters.put("beta", "0.1");
+		RunResults results = adapter.run(parameters, 5, 100, false);
+		
+		byte[] stdoutData = results.getStdoutData();
+		String stdoutString = new String(stdoutData, "UTF-8");
+		
+		assertEquals("-N5 -S100 -Dbeta=0.1", stdoutString);
 	}
 }
