cc94ac32026293acaaf0a5491c2f67b13a669c1c
ebaskerv <ebaskerv@e466becf-6918-0410-8e4b-ca7f87a03805>
2007-06-23 20:24:47 +0000

    minor changes, Sweep.java javadoc.
    git-svn-id: svn+ssh://battistini.cscs.lsa.umich.edu/users/gsweep/svn/gridsweeper@40 e466becf-6918-0410-8e4b-ca7f87a03805

--- trunk/src/com/edbaskerville/gridsweeper/parameters/Sweep.java
+++ trunk/src/com/edbaskerville/gridsweeper/parameters/Sweep.java
@@ -2,7 +2,23 @@ package com.edbaskerville.gridsweeper.parameters;
 
 import java.util.Random;
 
+/**
+ * The root interface for parameter sweeps. Defines one method, <code>generateMaps()</code>,
+ * which generates parameter name/value maps from parameter sweep settings.
+ * @author Ed Baskerville 
+ */
 public interface Sweep
 {
+	/**
+	 * Generates an enumerated list of {@link ParameterMap} objects, each of which
+	 * maps parameters to values. The resulting maps can be passed on to model runs through
+	 * {@link com.edbaskerville.gridsweeper.Adapter} objects.
+	 * @param rng The random number generator to use (for stochastic sweeps).
+	 * @return A list of {@code ParameterMap} objects, one for each 
+	 * @throws SweepLengthException If a length requirement&mdash;e.g., child sweeps
+	 * must be the same length&mdash;is violated. 
+	 * @throws DuplicateParameterException If child sweeps generate duplicate/conflicting
+	 * settings for a parameter.
+	 */
 	public java.util.List<ParameterMap> generateMaps(Random rng) throws SweepLengthException, DuplicateParameterException;
 }
