9d4409c8198c3678c557dc5df3d512e794ae53a8
Ed Baskerville <Ed Baskerville@e466becf-6918-0410-8e4b-ca7f87a03805>
2007-07-06 07:19:03 +0000

    Removed interface implementation List<Sweep> from ListSweep.
    git-svn-id: svn+ssh://battistini.cscs.lsa.umich.edu/users/gsweep/svn/gridsweeper@51 e466becf-6918-0410-8e4b-ca7f87a03805

--- trunk/src/com/edbaskerville/gridsweeper/parameters/ListSweep.java
+++ trunk/src/com/edbaskerville/gridsweeper/parameters/ListSweep.java
@@ -8,9 +8,8 @@ import java.util.*;
  * @author ebaskerv
  *
  */
-public class ListSweep extends SingleSweep implements List<String>
+public class ListSweep extends SingleSweep
 {
-	// TODO: remove this weird "implements List<String>".
 	private StringList values;
 	
 	public ListSweep(String name, StringList values)
@@ -40,115 +39,5 @@ public class ListSweep extends SingleSweep implements List<String>
 	public boolean add(String o)
 	{
 		return values.add(o);
-	}
-
-	public void add(int index, String element)
-	{
-		values.add(index, element);
-	}
-
-	public boolean addAll(Collection<? extends String> c)
-	{
-		return values.addAll(c);
-	}
-
-	public boolean addAll(int index, Collection<? extends String> c)
-	{
-		return values.addAll(index, c);
-	}
-
-	public void clear()
-	{
-		values.clear();
-	}
-
-	public boolean contains(Object o)
-	{
-		return values.contains(o);
-	}
-
-	public boolean containsAll(Collection<?> c)
-	{
-		return values.containsAll(c);
-	}
-
-	public String get(int index)
-	{
-		return values.get(index);
-	}
-
-	public int indexOf(Object o)
-	{
-		return values.indexOf(o);
-	}
-
-	public boolean isEmpty()
-	{
-		return values.isEmpty();
-	}
-
-	public Iterator<String> iterator()
-	{
-		return values.iterator();
-	}
-
-	public int lastIndexOf(Object o)
-	{
-		return values.lastIndexOf(o);
-	}
-
-	public ListIterator<String> listIterator()
-	{
-		return values.listIterator();
-	}
-
-	public ListIterator<String> listIterator(int index)
-	{
-		return values.listIterator(index);
-	}
-
-	public boolean remove(Object o)
-	{
-		return values.remove(o);
-	}
-
-	public String remove(int index)
-	{
-		return values.remove(index);
-	}
-
-	public boolean removeAll(Collection<?> c)
-	{
-		return values.removeAll(c);
-	}
-
-	public boolean retainAll(Collection<?> c)
-	{
-		return values.retainAll(c);
-	}
-
-	public String set(int index, String element)
-	{
-		return values.set(index, element);
-	}
-
-	public int size()
-	{
-		return values.size();
-	}
-
-	public List<String> subList(int fromIndex, int toIndex)
-	{
-		return values.subList(fromIndex, toIndex);
-	}
-
-	public Object[] toArray()
-	{
-		return values.toArray();
-	}
-
-	public <T> T[] toArray(T[] a)
-	{
-		return null;
 	}
 }
